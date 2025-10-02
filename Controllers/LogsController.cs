aee188290fbae4e185a2a07667e520dc2a65ac3d
harris <harrisvanweert@gmail.com>
2025-10-02 18:45:50 +0200

    added a  post create log endpoint

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,29 @@
+﻿using LoggerAPI.Models;
+using LoggerAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogsController : ControllerBase
+    {
+        private readonly LogRepository _logRepository;
+
+        public LogsController(LogRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLog([FromBody] LogEntry log)
+        {
+            if (log == null || string.IsNullOrEmpty(log.Message) || string.IsNullOrEmpty(log.LogLevel))
+                return BadRequest("LogLevel and Message are required");
+
+            await _logRepository.AddLogAsync(log);
+            return Ok(new { message = "Log created", logid = log.Id});
+        }
+
+    }
+}
---
+++ Repositories/LogRepository.cs
@@ -0,0 +1,23 @@
+﻿using LoggerAPI.Data;
+using LoggerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace LoggerAPI.Repositories
+{
+    public class LogRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddLogAsync(LogEntry log)
+        {
+            _context.LogEntries.Add(log);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
